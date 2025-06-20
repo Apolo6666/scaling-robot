72189a9094b7894a6f18fba7c2ec1c8b14494085
Apolo6666 <danieladamowicz14@gmail.com>
2025-06-20 13:24:31 +0300

    Create imghdr.py

---
+++ imghdr.py
@@ -0,0 +1,7 @@
+# stub for imghdr standard library module
+# so that telegram.files.inputfile import imghdr will succeed
+def what(file, h=None):
+    """
+    Stub implementation: always return None.
+    """
+    return None
