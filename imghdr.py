a9726e9cd4b950d2f60a01b8bc4ac5c86d4342f3
Apolo6666 <danieladamowicz14@gmail.com>
2025-06-22 23:09:22 +0300

    Delete imghdr.py

--- imghdr.py
+++
@@ -1,7 +0,0 @@
-# stub for imghdr standard library module
-# so that telegram.files.inputfile import imghdr will succeed
-def what(file, h=None):
-    """
-    Stub implementation: always return None.
-    """
-    return None
